eeecbf376a842a782ccbc1d69a9c3fa4e7c449d8
jfgilbertqc <moi@jfgilbert.ca>
2016-02-16 16:13:25 -0500

    First commit

---
+++ ByteHelper.Tests/ByteExtensionsTests.cs
@@ -0,0 +1,156 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ByteHelper.Tests
+{
+
+    [TestClass]
+    public class ByteExtensionsTests
+    {
+
+        #region GetBytes Tests
+
+
+        [TestMethod]
+        public void GivenNullByteArray_WhenGetBytes_ReturnEmptyByteArray()
+        {
+            byte[] bytes = null;
+            // ReSharper disable once ExpressionIsAlwaysNull
+            var result = bytes.GetBytes(0, 1);
+            result.Length.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void GivenEmptyByteArray_WhenGetBytes_ReturnEmptyByteArray()
+        {
+            byte[] bytes = { };
+            var result = bytes.GetBytes(0, 1);
+            result.Length.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void GivenStartAtLowerThanZero_WhenGetBytes_ReturnEmptyByteArray()
+        {
+            byte[] bytes = { };
+
+            var result = bytes.GetBytes(0, -1);
+
+            result.Length.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void GivenStartAtEqualsToOrGreaterThanArrayLength_WhenGetBytes_ReturnEmptyByteArray()
+        {
+            byte[] bytes = { 1 };
+
+            var result = bytes.GetBytes(1, 1);
+
+            result.Length.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void GivenStartAtPlusLengthGreaterThanArrayLength_WhenGetBytes_ReturnEmptyByteArray()
+        {
+            byte[] bytes = { 1, 2 };
+
+            var result = bytes.GetBytes(0, 3);
+
+            result.Length.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void GivenDataInArray_WhenGetBytes_ReturnByteArray()
+        {
+            const int expectedNumber1 = 1;
+            const int expectedNumber2 = 2;
+            const int expectedNumber3 = 3;
+            byte[] bytes = { expectedNumber1, expectedNumber2, expectedNumber3 };
+
+            var result = bytes.GetBytes(0, 3);
+
+            result.Length.Should().Be(3);
+            result[0].Should().Be(expectedNumber1);
+            result[1].Should().Be(expectedNumber2);
+            result[2].Should().Be(expectedNumber3);
+
+            result = bytes.GetBytes(2, 1);
+
+            result.Length.Should().Be(1);
+            result[0].Should().Be(expectedNumber3);
+
+            result = bytes.GetBytes(0, 1);
+
+            result.Length.Should().Be(1);
+            result[0].Should().Be(expectedNumber1);
+
+            result = bytes.GetBytes(1, 1);
+
+            result.Length.Should().Be(1);
+            result[0].Should().Be(expectedNumber2);
+        }
+
+        #endregion
+
+        #region AppendBytes tests
+
+        [TestMethod]
+        public void GivenTwoNullArrays_WhenAppendBytes_ReturnsEmptyArray()
+        {
+
+            byte[] firstArray = null;
+
+            // ReSharper disable once ExpressionIsAlwaysNull
+            var result = firstArray.AppendBytes(null);
+
+            result.Should().NotBeNull();
+            result.Length.Should().Be(0);
+
+        }
+
+        [TestMethod]
+        public void GivenNullSource_WhenAppendBytes_ReturnsArray()
+        {
+
+            byte[] firstArray = null;
+            byte[] secondArray = { 1 };
+
+            // ReSharper disable once ExpressionIsAlwaysNull
+            var result = firstArray.AppendBytes(secondArray);
+
+            result.Length.Should().Be(1);
+            result[0].Should().Be(1);
+
+        }
+
+        [TestMethod]
+        public void GivenNullBytesToAppend_WhenAppendBytes_ReturnsArray()
+        {
+
+            byte[] firstArray = { 1 };
+
+            var result = firstArray.AppendBytes(null);
+
+            result.Length.Should().Be(1);
+            result[0].Should().Be(1);
+
+        }
+
+        [TestMethod]
+        public void GivenTwoArrays_WhenAppendBytes_ReturnsArray()
+        {
+
+            byte[] firstArray = { 1 };
+            byte[] secondArray = { 2 };
+
+            var result = firstArray.AppendBytes(secondArray);
+
+            result.Length.Should().Be(2);
+            result[0].Should().Be(1);
+            result[1].Should().Be(2);
+
+        }
+
+        #endregion
+
+    }
+}
---
+++ ByteHelper/ByteExtensions.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+
+namespace ByteHelper
+{
+    public static class ByteExtensions
+    {
+
+        /// <summary>
+        /// Get a subset of bytes from a byte array
+        /// </summary>
+        /// <param name="source">The source byte array</param>
+        /// <param name="startAt">The index of the first byte to get.</param>
+        /// <param name="length">The length of bytes to get</param>
+        /// <returns>An array of bytes.</returns>
+        /// <remarks>If the source byte array is null or empty, or if the index and/or length are greater than the
+        /// number of bytes in the array, it will return an empty array.</remarks>
+        public static byte[] GetBytes(this byte[] source, int startAt, int length)
+        {
+            if (source == null || startAt + length > source.Length || length < 1)
+            {
+                return new byte[] { };
+            }
+
+            var bytes = new byte[length];
+            Buffer.BlockCopy(source, startAt, bytes, 0, length);
+            return bytes;
+
+        }
+
+        public static byte[] AppendBytes(this byte[] source, byte[] bytesToAppend)
+        {
+            if (source == null && bytesToAppend == null)
+            {
+                return new byte[] { };
+            }
+
+            if (source == null)
+            {
+                return bytesToAppend;
+            }
+
+            if (bytesToAppend == null)
+            {
+                return source;
+            }
+
+            var bytes = new byte[source.Length + bytesToAppend.Length];
+            Array.Copy(source, bytes, source.Length);
+            Array.Copy(bytesToAppend, 0, bytes, source.Length, bytesToAppend.Length);
+
+            return bytes;
+
+        }
+    }
+}
